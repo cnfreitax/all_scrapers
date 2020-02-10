d26f9cfa77db7a66f1737d224fc7adde55fce65d
cnfreitax <60153670+cnfreitax@users.noreply.github.com>
2020-02-10 17:05:31 -0300

    Add files via upload

--- reclame_aqui_rappi.py
+++ reclame_aqui_rappi.py
@@ -1,17 +1,23 @@
 import os
 import csv
+import time
 from selenium import webdriver
+from time import sleep
 from bs4 import BeautifulSoup as bs
 
-cols = ['titulo', 'comentario', 'local', 'data', 'respondido']
+start = time.time()
+
+cols = ['titulo', 'reclamacao', 'localizacao']
+
+main_url = 'https://www.reclameaqui.com.br'
+alvo = 'empresa/rappi_179520/lista-reclamacoes/?categoria=0000000000000311'
+site_scp = f'{main_url}/{alvo}'
+reclamacao_app = '&categoria=0000000000000311'
+
+page = #qtd paginas
 
 if not os.path.exists('scrape_rappi_csv'):
     os.makedirs('scrape_rappi_csv')
-
-main_url = 'https://www.reclameaqui.com.br'
-alvo = 'empresa/rappi_179520/lista-reclamacoes/'
-site_scp = f'{main_url}/{alvo}' 
-
 
 options = webdriver.ChromeOptions()
 options.add_argument('--no-sandbox')
@@ -21,14 +27,39 @@ prefs = {
 }
 
 options.add_experimental_option('prefs', prefs)
-browser = webdriver.Chrome('chromedriver', options=options)
+browser = webdriver.Chrome('/home/ekoar/chromedriver')
+
+lista_links = []
 
 browser.get(site_scp)
-
 bsObj = bs(browser.page_source, 'html.parser')
 
-#listaObjs = bsObj.find('li', {'class':'ng-scope'}).find_all('a')
-print(f'*************************')
-listaObjs = bsObj.find('ul', {'class': 'complain-list'}).find_all('li')
-print(listaObjs)
+with open(f'raclame_aqui_rappi.csv', 'a') as file:
+    dw = csv.DictWriter(file, fieldnames=cols, lineterminator='\n')
+    dw.writeheader()
+    for i in range(page):
+        i = str(i + 1)
+        browser.get(f'https://www.reclameaqui.com.br/empresa/rappi_179520/lista-reclamacoes/?pagina={i}{reclamacao_app}')
+        bsObj = bs(browser.page_source, 'html.parser')
+        sleep(2)
+        boxes = bsObj.find_all('a', {'link-complain-id-complains'})
+        lista_links = [box.get('href') for box in boxes]
+        page_links = [f'{main_url}{link}' for link in lista_links]
+        sleep(0.5)
+        for link in page_links:
+            browser.get(link)
+            sleep(1.5)
+            bs_page = bs(browser.page_source, 'html.parser')
+            titulo = bs_page.find('h1', {'class':'ng-binding'}).text.strip()
+            reclamacao = bs_page.find('div', {'class':'complain-body'}).find('p').text.strip()
+            localizacao = bs_page.find('li', {'class':'ng-binding'}).text.strip()
 
+            dw.writerow({
+                            'titulo': titulo, 'reclamacao': reclamacao,
+                            'localizacao': localizacao
+                        })
+
+end = time.time()
+browser.quit()
+
+print(f'Uma página com 10 reclamações foi raspada em {end - start} segundos.' )
