fde24ae85be7eb161afa22ccbe44a9d1dc97a8ca
cnfreitax <60153670+cnfreitax@users.noreply.github.com>
2020-02-13 11:04:04 -0300

    Add files via upload

--- reclame_aqui_rappi.py
+++ reclame_aqui_rappi.py
@@ -8,14 +8,17 @@ from bs4 import BeautifulSoup as bs
 
 start = time.time()
 
-cols = ['titulo', 'reclamacao', 'localizacao']
+cols = [
+        'titulo', 'reclamacao', 'localizacao',
+         'status', 'classificador', 'data_reclamacao'
+       ]
 
-main_url = 'https://www.reclameaqui.com.br'
-alvo = 'empresa/rappi_179520/lista-reclamacoes/?categoria=0000000000000311'
-site_scp = f'{main_url}/{alvo}'
-reclamacao_app = '&categoria=0000000000000311'
+main_url  = 'https://www.reclameaqui.com.br'
+alvo      = 'empresa/rappi_179520/lista-reclamacoes/?categoria=0000000000000311'
+site_scp  = f'{main_url}/{alvo}'
+categoria = '&categoria=0000000000000311'
 
-page = 3056
+page = 3074
 
 if not os.path.exists('scrape_rappi_csv'):
     os.makedirs('scrape_rappi_csv')
@@ -26,9 +29,9 @@ prefs = {
     'profile.default_content_setting_values.automatic_downloads': 1,
     'profile.managed_default_content_settings.images': 2
 }
-
 options.add_experimental_option('prefs', prefs)
-browser = webdriver.Chrome('/home/ekoar/chromedriver')
+
+browser = webdriver.Chrome('/home/ekoar/chromedriver', chrome_options=options)
 
 lista_links = []
 
@@ -40,7 +43,7 @@ with open(f'raclame_aqui_rappi.csv', 'a') as file:
     dw.writeheader()
     for i in tqdm(range(page)):
         i = str(i + 1)
-        browser.get(f'https://www.reclameaqui.com.br/empresa/rappi_179520/lista-reclamacoes/?pagina={i}{reclamacao_app}')
+        browser.get(f'https://www.reclameaqui.com.br/empresa/rappi_179520/lista-reclamacoes/?pagina={i}{categoria}')
         bsObj = bs(browser.page_source, 'html.parser')
         sleep(2)
         boxes = bsObj.find_all('a', {'link-complain-id-complains'})
@@ -48,17 +51,34 @@ with open(f'raclame_aqui_rappi.csv', 'a') as file:
         page_links = [f'{main_url}{link}' for link in lista_links]
         sleep(0.5)
         for link in page_links:
+            lista_palavras_chaves = []
+
             browser.get(link)
-            sleep(1.5)
+            sleep(0.3)
             bs_page = bs(browser.page_source, 'html.parser')
-            titulo = bs_page.find('h1', {'class':'ng-binding'}).text.strip()
-            reclamacao = bs_page.find('div', {'class':'complain-body'}).find('p').text.strip()
+            sleep(2)
+            titulo = bs_page.find('h1', {'class':'ng-binding'})
+            titulo = titulo.text.strip()
+            reclamacao = bs_page.find('div', {'class':'complain-body'}).find('p')
+            reclamacao = reclamacao.text.strip()
             localizacao = bs_page.find('li', {'class':'ng-binding'}).text.strip()
+            status = browser.find_element_by_xpath('//*[@id="complain-detail"]/div/div[1]/div[2]/div/div[1]/div[2]/div[2]/div/span/img')
+            status = status.get_attribute('title')
+            palavras_chaves = bs_page.find_all('li', {'class':'ng-scope'})
+            for classificador in palavras_chaves:
+                classificador = classificador.text.strip()
+                lista_palavras_chaves.append(classificador)
+            classificador = str(lista_palavras_chaves).strip('[]').replace(',', ' -')
+            data_reclamacao = browser.find_element_by_xpath('//li[@class = "ng-binding"][2]')
+            data_reclamacao = data_reclamacao.text
 
             dw.writerow({
                             'titulo': titulo, 'reclamacao': reclamacao,
-                            'localizacao': localizacao
+                            'localizacao': localizacao, 'status': status,
+                            'classificador': classificador, 'data_reclamacao': data_reclamacao
                         })
+
+            lista_palavras_chaves = []
 
 end = time.time()
 browser.quit()
